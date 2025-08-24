cdb508f69e4c23010b1b352b6adf794e722c7b94
sathiesh-sk <sathieshsk398@gmail.com>
2025-08-24 18:55:52 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,21 @@
+class demo{
+    public static void main(String args[])
+    {
+        String a = "Red";
+   
+        
+        if(a == "Red")
+        {
+            System.out.print("Stop");
+        }
+        else if(a == "Yellow")
+        {
+            System.out.print("Get Ready");
+        }  
+        else if(a == "Green")
+        {
+            System.out.print("Go");
+        }
+
+    }
+}
