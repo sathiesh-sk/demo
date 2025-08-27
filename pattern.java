f6f50445dd4166cac01e5794305dd57f3392eb03
sathiesh-sk <sathieshsk398@gmail.com>
2025-08-27 19:06:55 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+class Num{
+    public static void main(String args[])
+    {
+        Scanner sk = new Scanner(System.in);
+        System.out.print("Enter the Number :");
+        int n = sk.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}                 
+
+import java.util.Scanner;
+class Num{
+    public static void main(String args[])
+    {
+        Scanner sk = new Scanner(System.in);
+        System.out.print("Enter the Number :");
+        int n = sk.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}      
+
+import java.util.Scanner;
+class Num{
+    public static void main(String args[])
+    {
+        Scanner sk = new Scanner(System.in);
+        System.out.print("Enter the Number :");
+        int n = sk.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}   
+
+import java.util.Scanner;
+class Num{
+    public static void main(String args[])
+    {
+        Scanner sk = new Scanner(System.in);
+        System.out.print("Enter the Number :");
+        int n = sk.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}     
+
+import java.util.Scanner;
+class Num{
+    public static void main(String args[])
+    {
+        Scanner sk = new Scanner(System.in);
+        System.out.print("Enter the Number :");
+        int n = sk.nextInt();
+
+        for(int i=n;i>=1;i--){
+            for(int j=n;j>=n-i+1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}    
+
+import java.util.Scanner;
+class Num{
+    public static void main(String args[])
+    {
+        Scanner sk = new Scanner(System.in);
+        System.out.print("Enter the Number :");
+        int n = sk.nextInt();
+
+        for(int i=n;i>=1;i--){
+            for(int j=n;j>=n-i+1;j--){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}    
